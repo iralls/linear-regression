91549cb8fb650bf750f68456c8521486cc59a2de
Ian Ralls <ianralls19@gmail.com>
2015-08-22 16:41:34 -0400

    first rev of gradient descent and ordinary least squares

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # linear-regression
+
+From some coursework I've been doing, more notes for myself than anything
+
+#### LeastSquares
+
+Implements ordinary least squares for linear regression
+
+```python
+training_data = [[1,3], [4,6], [7,10], [8,12], [9,14]]
+l = LeastSquares(training_data)
+l.calculate()
+print l.get_formula()
+1.3551*x + 1.1402
+```
+
+#### GradientDescent
+
+Implements gradient descent for linear regression
+
+You choose the intial values for `m` and `b` in the hypothesis function `y = m * x + b`
+
+```python
+training_data = [[1,3], [4,6], [7,10], [8,12], [9,14]]
+l = GradientDescent(training_data)
+l.calculate()
+print l.get_formula()
+1.4669*x + 0.2204
+
+training_data = [[1,3], [4,6], [7,10], [8,12], [9,14]]
+l = GradientDescent(training_data, 1, 1)
+l.calculate()
+print l.get_formula()
+1.3515*x + 1.0497
+```
---
+++ single_variable.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+
+from __future__ import division
+
+class LeastSquares(object):
+	"""
+	implements linear regression using ordinary least squares
+
+	training_data must be in form [[x1, y1], [x2, y2]]
+
+	a good explanation: http://www.ismll.uni-hildesheim.de/lehre/ml-07w/skript/ml-2up-01-linearregression.pdf
+	"""
+
+	def __init__(self, training_data):
+		self.training_data = training_data
+
+	def calculate(self):
+		""" calculate linear regression formula """
+
+		mean_x = sum([i[0] for i in self.training_data]) / len(self.training_data)
+		mean_y = sum([i[1] for i in self.training_data]) / len(self.training_data)
+
+		quotient = 0
+		divisor = 0
+		for i in self.training_data:
+			quotient += (i[0] - mean_x) * (i[1] - mean_y)
+			divisor += (i[0] - mean_x) ** 2
+
+		self.m = quotient / divisor
+		self.b = mean_y - self.m * mean_x
+
+	def get_formula(self):
+		""" return the formula after running gradient descent """
+
+		return "{0}*x + {1}".format(round(self.m, 4), round(self.b, 4))
+
+class GradientDescent(object):
+	"""
+	implements linear regression using gradient descent
+
+	training_data must be in form [[x1, y1], [x2, y2]]
+	"""
+
+	def __init__(self, training_data, init_m=0, init_b=0, learning_rate=0.005, max_iterations=5000, max_cost_diff=0.005):
+		self.training_data = training_data
+
+		# initial value for m in y = m*x + b
+		self.init_m = init_m
+
+		# initial value for b in y = m*x + b
+		self.init_b = init_b
+		self.learning_rate = learning_rate
+
+		# max number of iterations to follow gradient descent before stopping
+		self.max_iterations = max_iterations
+
+		# max difference in the cost between descents before stopping
+		# **little to no difference indicates convergence
+		self.max_cost_diff = max_cost_diff
+
+	def hypothesis(self, m, x, b):
+		""" calculate hypothesis function """
+
+		return m * x + b
+
+	def cost(self, m, b):
+		""" calculate the cost function for values of m and b """
+
+		total = 0
+		for i in self.training_data:
+			total += (self.hypothesis(m, i[0], b) - i[1]) ** 2
+
+		return total / (2 * len(self.training_data))
+
+	def calculate(self):
+		""" use gradient descent to calculate linear regression formula """
+
+		count = 0
+		last_cost = 0
+
+		b = self.init_b
+		m = self.init_m
+
+		while True:
+			tmp_b = 0
+			tmp_m = 0
+
+			for i in self.training_data:
+				tmp_b += self.hypothesis(m, i[0], b) - i[1]
+				tmp_m += (self.hypothesis(m, i[0], b) - i[1]) * i[0]
+
+			b -= (self.learning_rate * tmp_b) / len(self.training_data)
+			m -= (self.learning_rate * tmp_m) / len(self.training_data)
+
+			count += 1
+			tmp_cost = self.cost(m, b)
+
+			if abs(tmp_cost - last_cost) <= self.max_cost_diff or count > self.max_iterations:
+				self.b = b
+				self.m = m
+				break
+			else:
+				last_cost = tmp_cost
+
+	def get_formula(self):
+		""" return the formula after running gradient descent """
+
+		return "{0}*x + {1}".format(round(self.m, 4), round(self.b, 4))
